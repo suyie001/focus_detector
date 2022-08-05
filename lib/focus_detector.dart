40acb89ba3d978f8441ff1cb8e7c399ef8b3e38e
suyie001 <suyie001@gmail.com>
2022-08-05 10:55:19 +0800

    Update focus_detector.dart

--- lib/focus_detector.dart
+++ lib/focus_detector.dart
@@ -162,4 +162,12 @@ class _FocusDetectorState extends State<FocusDetector>
     _ambiguate(WidgetsBinding.instance)?.removeObserver(this);
     super.dispose();
   }
+  
+  /// This allows a value of type T or T?
+  /// to be treated as a value of type T?.
+  ///
+  /// We use this so that APIs that have become
+  /// non-nullable can still be used with `!` and `?`
+  /// to support older versions of the API as well.
+  T? _ambiguate<T>(T? value) => value;
 }
